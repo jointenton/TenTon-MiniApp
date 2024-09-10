73f3d930c3bbbe4857f3ead0ca5aaba30bdb34c7
Dev Royale <unityekeoba18@gmail.com>
2024-09-10 21:09:57 +0100

    updated

--- Backend/utils.ts
+++ Backend/utils.ts
@@ -8,7 +8,7 @@ import {
   type ConversationFlavor,
 
 } from "@grammyjs/conversations";
-import { greetings } from ".";
+// import { greetings } from ".";
 import { ParseModeFlavor } from "@grammyjs/parse-mode";
 
 const TOKEN = '6395199784:AAEOXWvCJWBTNjsBvL1AL7Wap3UVMpMua9A';
